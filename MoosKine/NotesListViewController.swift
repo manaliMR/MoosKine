464551a039e5f7ddbbd721d3326cd98e05fd9949
manali78 <manalirami78@gmail.com>
2018-08-29 21:56:53 -0400

    tableView added

--- MoosKine/NotesListViewController.swift
+++ MoosKine/NotesListViewController.swift
@@ -103,6 +103,105 @@ class NotesListVewController: UIViewController, UITableViewDataSource {
         super.setEditing(editing, animated: animated)
         tableView.setEditing(editing, animated: animated)
     }
+    
+    // MARK: Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedResultsController.sections?[0].numberOfObjects ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let aNote = fetchedResultsController.object(at: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: NoteCell.defaultReuseIdentifier, for: indexPath) as! NoteCell
+    // Configure cell
+        cell.textPreviewLabel.attributedText = aNote.attributedText
+        
+        if let creationDate = aNote.creationDate {
+            cell.dateLabel.text = dateFormatter.string(from: creationDate)
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch  editingStyle {
+        case .delete: deleteNote(at: indexPath)
+        default: () // Unsupported
+        }
+    }
+    
+    // MARK: Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // If this is a NoteDetailsViewController, we'll configure its 'Note'
+        // anditd delete action
+        if let vc = segue.destination as? NoteDetailsViewContoller {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                vc.note = fetchedResultsController.object(at: indexPath)
+                vc.dataController = dataController
+                
+                vc.onDelete = { [weak self] in
+                    if let indexPath = self?.tableView.indexPathForSelectedRow {
+                        self?.deleteNote(at: indexPath)
+                        self?.navigationController?.popViewController(animated: true)
+                    }
+                }
+            }
+        }
+    }
+    
+    extension NotesListViewController:NSFetchedResultsControllerDelegate {
+        
+        func controller(_ controller:NSFetchedResultsController<NSFetcRequestResult>, didchange anyObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+            switch type {
+            case .insert:
+                tableView.insertRows(at: [newIndexPath!], with: .fade)
+                break
+            case .delete:
+                tableView.deleteRows(at: [indexPath!], with: .fade)
+                break
+            case .update:
+                tableView.reloadRows(at: [indexPath!], with: .fade)
+            case .move:
+                tableView.moveRow(at: indexPath!, to: newIndexPath!)
+            }
+        }
+        
+        func controller(_ controller:NSFetchedResultsController<NSFetchRequestResult> , didchange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+            let indexSet = IndexSet(integer: sectionIndex)
+            switch type {
+            case .insert: tableView.insertSections(indexSet, with: .fade)
+            case .delete: tableView.deleteSections(indexSet, with: .fade)
+            case .update, .move:
+                fatalError("Invalid change type in controller(_:didChange:atSectionIndex:for:). Only .insert or .deleet should be possible.")
+            }
+        }
+        
+        func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            tableView.beginUpdates()
+        }
+        
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchedRequestResult>) {
+            tableView.endUpdates()
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
